6fb74b39bb4a8c0e316b47e2b79cd21db0e9c1af
agul <agul64980223@qq.com>
2022-04-11 17:29:27 +0800

    55

--- bin/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(copy(dirname(__FILE__)."\ECDSA.dll.config",getcwd()."\ECDSA.config")){
-    echo "ok";
-}else{
-    echo "error";
-}
--- bin/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(copy(getcwd()."\ECDSA.config",dirname(__FILE__)."\ECDSA.dll.config")){
-    echo "ok";
-}else{
-    echo "error";
-}
