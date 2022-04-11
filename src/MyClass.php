61c14e4e4aa8bf89c8b7d312b68d27f640a49f40
agul <agul64980223@qq.com>
2022-04-11 16:02:52 +0800

    1545

--- src/MyClass.php
+++ src/MyClass.php
@@ -7,16 +7,16 @@ class MyClass
 {
     public static function init()
     {
-        if(copy(dirname(__FILE__)."ECDSA.dll.config",getcwd()."ECDSA.config")){
+        if(copy(dirname(__FILE__)."\ECDSA.dll.config",getcwd()."\ECDSA.config")){
             echo "ok";
         }else{
             echo "error";
         }
     }
-    
+
     public static function update()
     {
-        if(copy(getcwd()."ECDSA.config",dirname(__FILE__)."ECDSA.dll.config")){
+        if(copy(getcwd()."\ECDSA.config",dirname(__FILE__)."\ECDSA.dll.config")){
             echo "ok";
         }else{
             echo "error";
